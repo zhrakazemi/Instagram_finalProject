e3a2bb27276b9bec77e4834f4e13badad413d8bb
miladbila <btavakoli14@gmail.com>
2022-07-03 01:26:15 +0430

    adding followers , followings and posts ArrayLists to Account class

--- src/main/java/com/instagram_finalproject/Account.java
+++ src/main/java/com/instagram_finalproject/Account.java
@@ -2,53 +2,109 @@ package com.instagram_finalproject;
 
 import java.util.ArrayList;
 
-public class Account
-{
-    private String username ;
-    private String password ;
-    private String name ;
-    private String lastName ;
-    private String email ;
-    private String birthDate ;   //
-    private String bio ;
-
-    static ArrayList<Account> AllAccount = new ArrayList<>() ;
+public class Account {
+    private String username;
+    private String password;
+    private String name;
+    private String lastName;
+    private String email;
+    private String birthDate;   //
+    private String bio;
+    private ArrayList<Account> followings = new ArrayList<>();
+    private ArrayList<Account> followers = new ArrayList<>();
+    private ArrayList<Post> posts = new ArrayList<>();
+    static ArrayList<Account> AllAccount = new ArrayList<>();
 
 
 ////////////////////////////////////// Set & Get  //////////////////////////////////////
 
-    public String getUsername() { return username; }
+    public String getUsername() {
+        return username;
+    }
 
-    public void setUsername(String username) { this.username = username; }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-    public String getPassword() { return password; }
+    public String getPassword() {
+        return password;
+    }
 
-    public void setPassword(String password) { this.password = password; }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-    public void setName(String name) { this.name = name; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public String getLastName() { return lastName; }
+    public String getLastName() {
+        return lastName;
+    }
 
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-    public String getEmail() { return email; }
+    public String getEmail() {
+        return email;
+    }
 
-    public void setEmail(String email) { this.email = email; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    public String getBirthDate() { return birthDate; }
+    public String getBirthDate() {
+        return birthDate;
+    }
 
-    public void setBirthDate(String birthDate) { this.birthDate = birthDate; }
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
 
-    public String getBio() { return bio; }
+    public String getBio() {
+        return bio;
+    }
 
-    public void setBio(String bio) { this.bio = bio; }
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 
-    public static ArrayList<Account> getAllAccount() { return AllAccount; }
+    public static ArrayList<Account> getAllAccount() {
+        return AllAccount;
+    }
 
-    public static void setAllAccount(ArrayList<Account> allAccount) { AllAccount = allAccount; }
+    public static void setAllAccount(ArrayList<Account> allAccount) {
+        AllAccount = allAccount;
+    }
 
+    public ArrayList<Account> getFollowings() {
+        return followings;
+    }
+
+    public void setFollowings(ArrayList<Account> followings) {
+        this.followings = followings;
+    }
+
+    public ArrayList<Account> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(ArrayList<Account> followers) {
+        this.followers = followers;
+    }
+
+    public ArrayList<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(ArrayList<Post> posts) {
+        this.posts = posts;
+    }
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
     public Account(String username, String password, String name, String lastName, String email, String birthDate, String bio) {
