fc9ea44059820716a41489bb2b8d164f472fb15e
ParnianAlasvandian <parnian.alasvandian@gmail.com>
2022-07-03 22:44:23 +0430

    Not find

---
+++ src/main/java/com/instagram_finalproject/NotFind.java
@@ -0,0 +1,37 @@
+package com.instagram_finalproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+
+public class NotFind {
+
+    @FXML
+    private Button btn_OK;
+
+    @FXML
+    private Line l1;
+
+    @FXML
+    private Line l2;
+
+    @FXML
+    private Label lbl1;
+
+    @FXML
+    private Label lbl2;
+
+    @FXML
+    private Rectangle rec;
+
+    @FXML
+    void clickOK(MouseEvent event)
+    {
+        //برمیگرده به صفحه کاربر
+
+    }
+
+}
