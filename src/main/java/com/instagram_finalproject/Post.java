1b2f892c6278bcc3195c09875e9fd7e2de5d2590
ParnianAlasvandian <parnian.alasvandian@gmail.com>
2022-07-04 02:09:23 +0430

    update post class

--- src/main/java/com/instagram_finalproject/Post.java
+++ src/main/java/com/instagram_finalproject/Post.java
@@ -5,10 +5,13 @@ import java.util.ArrayList;
 public class Post
 {
     private String text ;
+    private int like ;
+    private int dislike ;
     ArrayList<Account> likeAccount = new ArrayList<>() ;       //برای لایک ها
     ArrayList<Account> disLikeAccount = new ArrayList<>() ;       //برای دیس لایک ها
     ArrayList<Comment> CommentAccount = new ArrayList<>() ;   //برای کامنت ها
     Account account;
+    static ArrayList<Post> popularPost = new ArrayList<>() ;
 
 ////////////////////////////////////// Set & Get  //////////////////////////////////////
 
@@ -36,10 +39,23 @@ public class Post
         return account;
     }
 
+    public int getLike() { return like; }
+
+    public void setLike(int like) { this.like = like; }
+
+    public int getDislike() { return dislike; }
+
+    public void setDislike(int dislike) { this.dislike = dislike; }
+
     public void setAccount(Account account) {
         this.account = account;
     }
-    /////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static ArrayList<Post> getPopularPost() { return popularPost; }
+
+    public static void setPopularPost(ArrayList<Post> popularPost) { Post.popularPost = popularPost; }
+
+///////////////////////////////////////////////////////////////////////////////////////////////
 
     public Post(String text,Account account) {
         this.text = text;
