5f041ff6067223acde89a83dcf5ddc90dd87c488
ParnianAlasvandian <parnian.alasvandian@gmail.com>
2022-07-03 22:43:58 +0430

    The password entered is incorrect

---
+++ src/main/java/com/instagram_finalproject/IncorrectPass.java
@@ -0,0 +1,45 @@
+package com.instagram_finalproject;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class IncorrectPass {
+
+    @FXML
+    private Button btn_ok;
+
+    @FXML
+    private Label lbl1;
+
+    @FXML
+    private Label lbl2;
+
+    @FXML
+    private Label lbl_InPass;
+
+    @FXML
+    private Rectangle rectangle;
+
+    @FXML
+    void clickOk(MouseEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("Register.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow() ;
+        Scene scene = new Scene(parent);
+        stage.setResizable(false);    //وقتی این فرم ایجاد شده کسی اجازه بزرگتر یا کوچکتر کردن اون رو نداشته باشه
+        //
+        stage.setTitle("Instagram");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
