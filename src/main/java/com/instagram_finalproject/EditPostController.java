042882e088c0a2390d1d43df9815fd4e44979238
zahra <zahra.k199002@gmail.com>
2022-07-04 01:12:54 +0430

    creating a class for editing post

---
+++ src/main/java/com/instagram_finalproject/EditPostController.java
@@ -0,0 +1,29 @@
+package com.instagram_finalproject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class EditPostController {
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnDone;
+
+    @FXML
+    private TextField txtNewText;
+
+    @FXML
+    void pressOnBtnBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pressOnBtnDone(ActionEvent event) {
+//update post text on data base
+    }
+
+}
