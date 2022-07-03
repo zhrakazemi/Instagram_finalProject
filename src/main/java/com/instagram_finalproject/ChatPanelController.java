62d16afbd0562efb84c92991ea18beb0cbe3bdd3
miladbila <btavakoli14@gmail.com>
2022-07-03 18:15:05 +0430

    Adding Message option

---
+++ src/main/java/com/instagram_finalproject/ChatPanelController.java
@@ -0,0 +1,66 @@
+package com.instagram_finalproject;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ChatPanelController {
+    ObservableList<String> observableList = FXCollections.observableArrayList();
+    public static Message message;
+
+    public void initialize() {
+        observableList.addAll(message.getText());
+        massagesListview.setItems(observableList);
+    }
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private ListView<String> massagesListview;
+
+    @FXML
+    private TextField messageTxt;
+
+    @FXML
+    private Button sendBtn;
+
+    @FXML
+    void onBackBtnClick(ActionEvent event) {
+        Stage stage = (Stage) backBtn.getScene().getWindow();
+        stage.close();
+        Stage primaryStage = new Stage();
+        AnchorPane root = null;
+        try {
+            root = (AnchorPane) FXMLLoader.load(HelloApplication.class.getResource("MessagePanel.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scene scene = new Scene(root, 800, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @FXML
+    void onSendBtnClick(ActionEvent event) {
+        if (message.getText().size() == 10) {
+            message.getText().remove(0);
+            message.getText().add(HelloApplication.loggedInAccount.getName() + " : " + messageTxt.getText());
+        } else {
+            message.getText().add(HelloApplication.loggedInAccount.getName() + " : " + messageTxt.getText());
+        }
+        observableList.add(HelloApplication.loggedInAccount.getName() + " : " + messageTxt.getText());
+        massagesListview.setItems(observableList);
+    }
+
+}
---
+++ src/main/java/com/instagram_finalproject/MessagePanelController.java
@@ -0,0 +1,80 @@
+package com.instagram_finalproject;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MessagePanelController {
+    public void initialize() {
+        ObservableList<String> observableList = FXCollections.observableArrayList();
+        for (Message a : HelloApplication.loggedInAccount.getMessages()) {
+            if (a.getPerson1() == HelloApplication.loggedInAccount)
+                observableList.add(a.getPerson2().getUsername());
+            else
+                observableList.add(a.getPerson1().getUsername());
+        }
+        usersListView.setItems(observableList);
+    }
+
+    @FXML
+    private Button addBtn;
+
+    @FXML
+    private TextField addNewUserTxt;
+
+    @FXML
+    private ListView<String> usersListView;
+
+    @FXML
+    void onAddBtnClick(ActionEvent event) {
+        for (Account a : Account.getAllAccount())
+            if (a.getUsername().equals(addNewUserTxt.getText())) {
+                Message temp = new Message(HelloApplication.loggedInAccount, a);
+                HelloApplication.loggedInAccount.getMessages().add(temp);
+                a.getMessages().add(temp);
+                ChatPanelController.message = temp;
+                Stage stage = (Stage) addBtn.getScene().getWindow();
+                stage.close();
+                Stage primaryStage = new Stage();
+                AnchorPane root = null;
+                try {
+                    root = (AnchorPane) FXMLLoader.load(HelloApplication.class.getResource("ChatPanel.fxml"));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                Scene scene = new Scene(root, 800, 500);
+                primaryStage.setScene(scene);
+                primaryStage.show();
+                break;
+            }
+    }
+
+    @FXML
+    void onUserListViewClick(MouseEvent event) {
+        Message temp = HelloApplication.loggedInAccount.getMessages().get(usersListView.getSelectionModel().getSelectedIndex());
+        Stage stage = (Stage) addBtn.getScene().getWindow();
+        stage.close();
+        Stage primaryStage = new Stage();
+        AnchorPane root = null;
+        try {
+            root = (AnchorPane) FXMLLoader.load(HelloApplication.class.getResource("ChatPanel.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scene scene = new Scene(root, 800, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
