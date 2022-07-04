0f4db6922df4602d30a493b34960e866c2635af5
ParnianAlasvandian <parnian.alasvandian@gmail.com>
2022-07-04 06:06:08 +0430

    update AddPostController class

--- src/main/java/com/instagram_finalproject/AddPostController.java
+++ src/main/java/com/instagram_finalproject/AddPostController.java
@@ -23,10 +23,15 @@ public class AddPostController {
 
     @FXML
     void pressOnBtnDone(ActionEvent event) {
+
         Post post = new Post(txtPostText.getText(), HelloApplication.loggedInAccount);
         HelloApplication.loggedInAccount.getPosts().add(post);
         if (!HelloApplication.loggedInAccount.isPrivateAccount()) {
             HelloApplication.allPosts.add(post);
+
+            String SQLCom = String.format("INSERT  INTO allposts (username, private, text, like, dislike) values ('%s', %s, '%s',%s, %s)",HelloApplication.loggedInAccount.getUsername(),0,txtPostText.getText(),0,0);
+            MySQLConnection sql = new MySQLConnection() ;
+            Boolean res  =  sql.ExecuteSQL(SQLCom) ;
         }
     }
 
