b93edcfe24daf216deef7c6b15903cc01ec4f57f
miladbila <btavakoli14@gmail.com>
2022-07-04 04:35:20 +0430

    fixing not showing problem

--- src/main/java/com/instagram_finalproject/ViewPostsForEditControllrer.java
+++ src/main/java/com/instagram_finalproject/ViewPostsForEditControllrer.java
@@ -1,6 +1,5 @@
 package com.instagram_finalproject;
 
-import com.sun.javafx.charts.Legend;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -13,21 +12,20 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import static com.instagram_finalproject.PostListController.posts;
-
 public class ViewPostsForEditControllrer {
 
     @FXML
-    private ListView<?> lvAlPosts;
+    private ListView<String> lvAlPosts;
     public void initialize() {
         ObservableList<String> observableList = FXCollections.observableArrayList();
         int i = 1;
         for (Post a : HelloApplication.loggedInAccount.getPosts())
             observableList.add(i++ + ". " + a.getText());
-        observableList.setAll(observableList);
+        lvAlPosts.setItems(observableList);
     }
-
+    @FXML
     void onPostsListviewClick(MouseEvent event) {
+        EditPostController.post=HelloApplication.loggedInAccount.getPosts().get(lvAlPosts.getSelectionModel().getSelectedIndex());
         Stage stage = (Stage) lvAlPosts.getScene().getWindow();
         stage.close();
         Stage primaryStage = new Stage();
